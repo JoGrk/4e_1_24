57ae15482f7ad9ac41b356fce8cb1335b3336c04
Jolanta Gryko <jgryko@gmail.com>
2025-04-02 12:42:02 +0200

    2.04

--- tizbd/03_raporty/06_zadania/kalendarz.php
+++ tizbd/03_raporty/06_zadania/kalendarz.php
@@ -1,10 +1,27 @@
+<?php
+$link= new mysqli('localhost', 'root', '', '4e_1_zadania');
+
+$sql="SELECT miesiac , rok
+FROM zadania
+WHERE dataZadania='2020-07-01'";
+$result=$link -> query($sql);
+$dates=$result -> fetch_all(1);
+
+$sql="SELECT dataZadania, wpis
+FROM zadania
+WHERE miesiac='lipiec'";
+$result=$link -> query($sql);
+$days=$result -> fetch_all(1);
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
+    
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mój kalendarz</title>
-    <link rel="stylesheet" href="styl5.css">
+    <link rel="stylesheet" href="styl6.css">
 </head>
 <body>
     <header>
@@ -15,6 +32,12 @@
         <section class="second">
             <h1>KALENDARZ</h1>
             <!-- skrypt 1 -->
+            <h3>
+            <?php
+                $date=$dates[0];
+                echo "miesiąc:  {$date['miesiac']}, rok: {$date['rok']}"
+            ?>
+            </h3>
         </section>
     </header>
 
@@ -27,3 +50,6 @@
     </footer>
 </body>
 </html>
+<?php
+$link -> close();
+?>
