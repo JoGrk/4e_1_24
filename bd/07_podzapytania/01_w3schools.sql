247839da90e726b51b087ea6bca03f4c674ca861
Jolanta Gryko <jgryko@gmail.com>
2025-05-26 14:24:31 +0200

    26.05

--- bd/07_podzapytania/01_w3schools.sql
+++ bd/07_podzapytania/01_w3schools.sql
@@ -38,11 +38,44 @@ FROM Products;
 -- 9. Wyświetl nazwiska i imiona klientów (Customer Name), którzy nie korzystali z firmy  Speedy Express
 
 -- 10. Wszystkie informacje o klientach, mających tę samą wartość w polu CustomerName (może wpisanych przez pomyłkę?)
+SELECT *
+FROM Customers
+WHERE CustomerName IN (SELECT CustomerName
+                        FROM Customers
+                        GROUP BY CustomerName
+                        HAVING COUNT(*)>1);
+
+INSERT INTO Customers (CustomerName)
+VALUES ('Wolski');
 
 -- update
 
 -- 11 Podnieś o 10% ceny produktów z kategorii  "Confections"
 
+UPDATE Products
+SET price=price*1.10
+WHERE categoryID=(SELECT categoryID
+FROM categories
+WHERE categoryName="confections");
+
 -- 12  Dla zamówień klientów z UK zmień spedytora na "United Package"
+UPDATE Orders
+SET ShipperID = (SELECT ShipperID
+                FROM shippers
+                WHERE ShipperName = "United Package")
+WHERE CustomerID IN (SELECT CustomerID 
+                    FROM Customers
+                    WHERE Country = "UK");
 
 -- 13. Dodaj nowy produkt z kategorii Confections dostarczony przez Exotic Liquid 
+
+INSERT INTO Products
+    (ProductName,categoryID,supplierID)
+VALUES
+    ('hlep',
+    (SELECT categoryID
+    FROM categories
+    WHERE categoryName='confections')
+    ,(SELECT supplierID
+    FROM suppliers
+    WHERE supplierName='exotic liquid'));
