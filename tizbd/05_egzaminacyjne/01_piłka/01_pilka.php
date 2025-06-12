30da23a0579f964775f91cab2aae2023d1407f81
Jolanta Gryko <jgryko@gmail.com>
2025-06-12 11:34:01 +0200

    12.06

---
+++ tizbd/05_egzaminacyjne/01_piłka/01_pilka.php
@@ -0,0 +1,54 @@
+<?php
+$link = new mysqli('localhost','root','','4e_1_pilka');
+$sql= "SELECT zespol1,zespol2,wynik,data_rozgrywki
+        FROM rozgrywka
+        WHERE zespol1='EVG'";
+$result = 
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rozgrywki futbolowe</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h2>Światowe rozgrywki piłkarskie</h2>
+        <img src="obraz1.jpg" alt="boisko">
+    </header>
+    
+    <section class="games">
+        <!-- skrypt 1 -->
+    </section>
+    
+    <main>
+        <h2>Reprezentacja Polski</h2>
+    </main>
+
+    <footer>
+        <section class="left">
+            <p>Podaj pozycję zawodników (1-bramkarze, 2-obrońcy, 3-pomocnicy, 4-napastnicy):</p>
+            <form action="" method="post">
+                <input type="number" name="position_f" id="position_f">
+                <button>Sprawdź</button>
+            </form>
+            <ul>
+                <!-- skrypt 2 -->
+            </ul>
+        </section>
+
+        <section class="right">
+            <img src="zad1.png" alt="piłkarz">
+            <p>Autor: 000000000000000</p>
+        </section>
+    </footer>
+
+</body>
+</html>
+
+<?php
+$link -> close();
+?>
